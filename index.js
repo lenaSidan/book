2878760d48844e2901e79cb1ebc5e2febe567cdb
Elena Sidanova <l.sidan@gmx.de>
2023-07-19 18:42:14 +0200

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const titleElement = document.getElementById("title");
+const yearElement = document.getElementById("year");
+const authorElement = document.getElementById("author");
+const genreElement = document.getElementById("genre");
+const imageElement = document.getElementById("book-image");
+async function loadBook() {
+    const res = await fetch ("https://lenasidan.github.io/book.api/book.json");
+    const obj = await res.json();
+    console.log(obj); //
+    const{title, year, author, genre, cover} = obj;
+    titleElement.textContent = title;
+    yearElement.textContent = year;
+    authorElement.textContent = author;
+    genreElement.textContent = genre;
+    imageElement.src = cover;
+    
+}
+loadBook();
